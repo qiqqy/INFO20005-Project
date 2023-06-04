fcb2a13023747c477fa671995901e8f196037938
Phoebe Li <prli@student.unimelb.edu.au>
2023-06-04 12:50:21 +1000

    side menu and side cart

---
+++ script.js
@@ -0,0 +1,15 @@
+function openmenu() {
+    document.getElementById("menu").style.width = "250px";
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.width = "0";
+}
+
+function opencart() {
+    document.getElementById("cart").style.width = "500px";
+}
+
+function closeCart() {
+    document.getElementById("cart").style.width = "0";
+}
